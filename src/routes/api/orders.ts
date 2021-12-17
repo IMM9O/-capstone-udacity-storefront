1c8b7518ef908981c4bb2adfeef3c3f94d42715f
IMM9O <islam.muhamed.90+github@gmail.com>
2021-12-17 11:09:28 +0200

    feat: add orders api handlers

--- src/routes/api/orders.ts
+++ src/routes/api/orders.ts
@@ -1,11 +1,34 @@
 import express from 'express';
+import {
+  addOrder,
+  deleteOrder,
+  getActiveOrders,
+  getCompletedOrders,
+  getOrder,
+  getOrders,
+  updateOrder,
+} from '../../services/orders';
 
 const orders = express.Router();
 
-// Current Order by user (args: user id)[token required]
-orders.get('/:userId', (req, res) =>
-  res.send(`List of orders for specific user = ${req.params.userId}`),
-);
+// Index [token required]
+orders.get('/:userId', getOrders);
 
+// Get Competed Orders
+orders.get('/completed/:userId', getCompletedOrders);
+// Get Active Orders
+orders.get('/active/:userId', getActiveOrders);
+
+// post request [token required]
+orders.post('/', addOrder);
+
+// show request [token required]
+orders.get('/:id', getOrder);
+
+// put request [token required]
+orders.put('/:id', updateOrder);
+
+// delete request [token required]
+orders.delete('/:id', deleteOrder);
 
 export default orders;
---
+++ src/services/orders.ts
@@ -0,0 +1,110 @@
+import { Request, Response } from 'express';
+import { OrderStore } from '../models/Order';
+
+/** CRUD Operations on order tables **/
+const store = new OrderStore();
+
+// Get User Orders
+export const getOrders = async (_req: Request, res: Response) => {
+  try {
+    // TODO: get user id from current loged in user
+    const user_id: number = parseInt(_req.params.userId);
+    const orders = await store.getUserOrders(user_id);
+    res.json(orders);
+  } catch (err) {
+    res.status(400);
+    res.json(err);
+  }
+};
+
+export const getCompletedOrders = async (
+  _req: Request,
+  res: Response,
+) => {
+  try {
+    // TODO: get user id from current loged in user
+    const user_id: number = parseInt(_req.params.userId);
+    const orders = await store.getUserCompletedOrders(user_id);
+    res.json(orders);
+  } catch (err) {
+    res.status(400);
+    res.json(err);
+  }
+};
+
+export const getActiveOrders = async (
+  _req: Request,
+  res: Response,
+) => {
+  try {
+    // TODO: get user id from current loged in user
+    const user_id: number = parseInt(_req.params.userId);
+    const orders = await store.getUserActiveOrders(user_id);
+    res.json(orders);
+  } catch (err) {
+    res.status(400);
+    res.json(err);
+  }
+};
+
+// Read
+export const getOrder = async (_req: Request, res: Response) => {
+  try {
+    const id: number = parseInt(_req.params.id);
+    const order = await store.show(id);
+    res.json(order);
+  } catch (err) {
+    res.status(400);
+    res.json(err);
+  }
+};
+
+// Create
+export const addOrder = async (_req: Request, res: Response) => {
+  // TODO: get user id from current loged in user
+  const user_id: number = parseInt(_req.body.user_id);
+  const status: string = _req.body.status;
+
+  try {
+    const addedOrder = await store.create({
+      user_id,
+      status,
+    });
+    res.json(addedOrder);
+  } catch (err) {
+    res.status(400);
+    res.json(err);
+  }
+};
+
+// Update
+export const updateOrder = async (_req: Request, res: Response) => {
+  const id: number = parseInt(_req.params.id);
+  // TODO: get user id from current loged in user
+  const user_id: number = parseInt(_req.body.user_id);
+  const status: string = _req.body.status;
+
+  try {
+    const updatedOrder = await store.update({
+      id,
+      user_id,
+      status,
+    });
+    res.json(updatedOrder);
+  } catch (err) {
+    res.status(400);
+    res.json(err);
+  }
+};
+
+// Delete
+export const deleteOrder = async (_req: Request, res: Response) => {
+  try {
+    const id: number = parseInt(_req.params.id);
+    const order = await store.delete(id);
+    res.json(order);
+  } catch (err) {
+    res.status(400);
+    res.json(err);
+  }
+};
